59b3fddab767210768eceac54397e91f5558af23
Shahrukh <msr.concordfly@gmail.com>
2018-04-08 09:42:29 +0530

    Added mixins, to the class based views

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
+from django.urls import path
 from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    # path(r'snippets/', views.snippet_list),
+    path(r'snippets/', views.SnippetList.as_view()),
+    path(r'snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
